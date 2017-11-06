950c460f928d221aa176f01e53ab235b863e2fd3
Sreeharsha Poluru <sreeharshapoluru@gmail.com>
2017-11-05 19:14:24 -0500

    Updated Robin's Theorem Visualization

--- RobinsTheoremVis.py
+++ RobinsTheoremVis.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt;
 
 plot1 = [0] * 9998;
 plot2 = [0] * 9998;
+plot3 = [0] * 9998;
 constant = 1.781974;
 for i in range(2,10000):
     lhs = 0;
@@ -12,10 +13,12 @@ for i in range(2,10000):
     for j in range(1,i+1):
         if(i%j == 0):
             lhs = lhs+j;
-    plot1[i-2] = lhs;
-    plot2[i-2] = rhs;
+    plot1[i-2] = i;
+    plot2[i-2] = lhs;
+    plot3[i-2] = rhs;
 
-plt.plot(plot2,plot1);
+plt.plot(plot1,plot2,'r');
+plt.plot(plot1,plot3,'b');
 plt.show();
 
 
